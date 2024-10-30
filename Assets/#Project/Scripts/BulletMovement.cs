89c0ccd97e7f8b45f98fef3c2a21af582d743ac7
Charlotte Mestan <cdmestan@outlook.com>
2024-10-30 14:37:41 +0100

    Small fix

--- Assets/#Project/Scripts/BulletMovement.cs
+++ Assets/#Project/Scripts/BulletMovement.cs
@@ -5,24 +5,13 @@ using UnityEngine;
 
 public class BulletMovement : MonoBehaviour
 {
-
     [SerializeField] float speed = 1f;
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
         transform.Translate(Vector2.right * Time.deltaTime * speed);
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Debug.Log("Collision");
-        if (collision.gameObject.GetComponent<ILimit>() != null) gameObject.SetActive(false); 
+       if (collision.gameObject.GetComponent<ILimit>() != null) gameObject.SetActive(false); 
     }
 }
--- Assets/#Project/Scripts/Wall.cs
+++ Assets/#Project/Scripts/Wall.cs
@@ -9,13 +9,10 @@ public interface ILimit
 
 public class Wall : MonoBehaviour, ILimit
 {
-    // Start is called before the first frame update
     void Start()
     {
         
     }
-
-    // Update is called once per frame
     void Update()
     {
         
