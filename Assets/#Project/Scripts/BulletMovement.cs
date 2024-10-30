1f64806d8c617573344fd1ac5f188c7fa9834d5d
Charlotte Mestan <cdmestan@outlook.com>
2024-10-30 16:18:30 +0100

    Add draft bullet direction

--- Assets/#Project/Scripts/BulletMovement.cs
+++ Assets/#Project/Scripts/BulletMovement.cs
@@ -6,12 +6,21 @@ using UnityEngine;
 public class BulletMovement : MonoBehaviour
 {
     [SerializeField] float speed = 1f;
+    private PlayerShoot player;
+    private Vector2 direction;
+
     void Update()
     {
-        transform.Translate(Vector2.right * Time.deltaTime * speed);
+        transform.Translate(direction * speed * Time.deltaTime);
     }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
        if (collision.gameObject.GetComponent<ILimit>() != null) gameObject.SetActive(false); 
+    }
+
+    public void SetDirection(Vector2 newDirection)
+    {
+        direction = newDirection;
     }
 }
--- Assets/#Project/Scripts/PlayerShoot.cs
+++ Assets/#Project/Scripts/PlayerShoot.cs
@@ -8,12 +8,14 @@ public class PlayerShoot : MonoBehaviour
 {
     [SerializeField] InputActionAsset inputActions;
     private InputAction shoot;
+    private InputAction shootDirection;
     [SerializeField] float shootingDelay = 0.5f;
     private float lastShotTime;
 
     private void Awake()
     {
         shoot = inputActions.FindActionMap("Player").FindAction("Shoot");
+        shootDirection = inputActions.FindActionMap("Player").FindAction("Shoot Direction");
     }
 
     private void OnEnable()
@@ -28,22 +30,31 @@ public class PlayerShoot : MonoBehaviour
 
     private void Update()
     {
+
+        Vector2 shootAmount = shootDirection.ReadValue<Vector2>().normalized;
+
         bool shootingInput = shoot.ReadValue<float>() == 1;
         if (shootingInput && Time.time >= lastShotTime + shootingDelay)
         {
-            Shoot();
+            Shoot(shootAmount);
             lastShotTime = Time.time;
         }
     }
 
-    private void Shoot()
+    //Next step: have the last direction memorized or a default direction
+    //so that when there's no direction pressed the bullet still has a direction
+
+    private void Shoot(Vector2 direction)
     {
+        Vector2 bulletDirection = direction;
         GameObject bullet = BulletPool.SharedInstance.GetPooledObject(); 
         if (bullet != null) 
         {
+            BulletMovement bulletMvmt = bullet.GetComponent<BulletMovement>();
             bullet.transform.position = transform.position;
             bullet.transform.rotation = transform.rotation;
             bullet.SetActive(true);
+            bulletMvmt.SetDirection(bulletDirection);
         }
     }
 }
