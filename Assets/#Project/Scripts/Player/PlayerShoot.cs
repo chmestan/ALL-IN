8126fe217fad3380d5a29ee5c582dc5399d13efd
Charlotte Mestan <cdmestan@outlook.com>
2024-11-09 14:50:00 +0100

    add input device change

--- Assets/#Project/Scripts/Player/PlayerShoot.cs
+++ Assets/#Project/Scripts/Player/PlayerShoot.cs
@@ -6,47 +6,102 @@ using UnityEngine.InputSystem;
 
 public class PlayerShoot : MonoBehaviour
 {
-    [Header ("Input Map")]
-    [SerializeField] InputActionAsset inputActions;
-
-    [Header ("Shooting"), Space (10f)]
-    [SerializeField] float shootingDelay = 0.5f;
     [SerializeField] Camera mainCamera;
-    
-    private InputAction shoot;
-    private InputAction shootDirection;
-    private Vector2 shootSnappedDir;
+
+    [Header("Input Map")]
+    [SerializeField] InputActionAsset inputActions;
+    private InputAction shootInput;
+    private InputAction shootDirectionInput;
+
+
+    [Header("Shooting"), Space(10f)]
+    [SerializeField] float shootingDelay = 0.5f;
+    private Vector2 shootDirection;
     private float lastShotTime;
-    private PlayerMovement playerMovement;
     private Vector2 LastDirection
     {
-        get { return playerMovement.lastDirection; } 
-        set { playerMovement.lastDirection = value; } 
+        get { return playerMovement.lastDirection; }
+        set { playerMovement.lastDirection = value; }
     }
 
+
+    private PlayerMovement playerMovement;
     private Animator anim;
 
-    [SerializeField, Space (20f)] bool debug = false;
+    [Header("Gamepad?"), Space(10f)]
+    [SerializeField] bool useGamepad = false;
+    [SerializeField, Space(20f)] bool debug = false;
 
     private void Awake()
     {
-        shoot = inputActions.FindActionMap("Player").FindAction("Shoot");
-        shootDirection = inputActions.FindActionMap("Player").FindAction("Shoot Direction");
         playerMovement = GetComponent<PlayerMovement>();
         anim = GetComponent<Animator>();
 
         if (mainCamera == null)
             mainCamera = Camera.main;
+
+        // Subscribe to device change events
+        InputSystem.onDeviceChange += OnDeviceChange;
+        
+        DetectCurrentInputMode();
+        SwitchInputMap();
     }
 
     private void OnEnable()
     {
-        inputActions.FindActionMap("Player").Enable();
+        EnableInputMap();
     }
 
     private void OnDisable()
     {
-        inputActions.FindActionMap("Player").Disable();
+        DisableInputMap();
+        InputSystem.onDeviceChange -= OnDeviceChange;
+    }
+
+    private void OnDeviceChange(InputDevice device, InputDeviceChange change)
+    {
+        if (change == InputDeviceChange.Added || change == InputDeviceChange.Removed)
+        {
+            DetectCurrentInputMode();
+            SwitchInputMap();
+        }
+    }
+
+    private void DetectCurrentInputMode()
+    {
+        useGamepad = (Gamepad.current != null); 
+    }
+
+    private void SwitchInputMap()
+    {
+        if (useGamepad)
+        {
+            inputActions.FindActionMap("PlayerKeyboard").Disable();
+            inputActions.FindActionMap("PlayerGamepad").Enable();
+            shootInput = inputActions.FindActionMap("PlayerGamepad").FindAction("Shoot");
+            shootDirectionInput = inputActions.FindActionMap("PlayerGamepad").FindAction("Shoot Direction");
+        }
+        else
+        {
+            inputActions.FindActionMap("PlayerGamepad").Disable();
+            inputActions.FindActionMap("PlayerKeyboard").Enable();
+            shootInput = inputActions.FindActionMap("PlayerKeyboard").FindAction("Shoot");
+            shootDirectionInput = inputActions.FindActionMap("PlayerKeyboard").FindAction("Shoot Direction");
+        }
+    }
+
+    private void EnableInputMap()
+    {
+        if (useGamepad)
+            inputActions.FindActionMap("PlayerGamepad").Enable();
+        else
+            inputActions.FindActionMap("PlayerKeyboard").Enable();
+    }
+
+    private void DisableInputMap()
+    {
+        inputActions.FindActionMap("PlayerKeyboard").Disable();
+        inputActions.FindActionMap("PlayerGamepad").Disable();
     }
 
     private void Update()
@@ -58,57 +113,58 @@ public class PlayerShoot : MonoBehaviour
 
     private void ShootingDirection()
     {
-        Vector2 mousePos = mainCamera.ScreenToWorldPoint(Mouse.current.position.ReadValue());
-        Vector2 directionToMouse = (mousePos - (Vector2)transform.position).normalized;
-        shootSnappedDir = SnapToEightDirections(directionToMouse);
-
-        bool shootingInput = (shoot.ReadValue<float>() == 1);
-        if (shootingInput && Time.time >= lastShotTime + shootingDelay)
+        if (!useGamepad)
         {
-            Shoot(shootSnappedDir);
-            lastShotTime = Time.time;
+            Vector2 mousePos = mainCamera.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+            shootDirection = (mousePos - (Vector2)transform.position).normalized; // direction to mouse
+        }
+        else
+        {
+            shootDirection = shootDirectionInput.ReadValue<Vector2>().normalized;
+            if (shootDirection != Vector2.zero)
+            {
+                LastDirection = shootDirection;
+            }
         }
 
-        LastDirection = shootSnappedDir;
+        bool shootingInput = (shootInput.ReadValue<float>() == 1);
+        if (shootingInput && Time.time >= lastShotTime + shootingDelay)
+        {
+            Shoot(LastDirection);
+            lastShotTime = Time.time;
+        }
     }
 
     private Vector2 SnapToEightDirections(Vector2 direction)
     {
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-        // Atan allows to find the angle between the positive X-axis and a(y,x) vector
-        // It returns an angle in radians and [ * Mathf.Rad2Deg ] converts it to degrees
         float snappedAngle = Mathf.Round(angle / 45f) * 45f;
-        // tells us which 45 degree angle we're closest to
         return new Vector2(Mathf.Cos(snappedAngle * Mathf.Deg2Rad), Mathf.Sin(snappedAngle * Mathf.Deg2Rad)).normalized;
-        // converts our angle to a directional vector
     }
 
     private void Shoot(Vector2 direction)
     {
-        Vector2 bulletDirection = direction;
-        GameObject bullet = BulletPool.SharedInstance.GetPooledObject(); 
-        if (bullet != null) 
+        GameObject bullet = BulletPool.SharedInstance.GetPooledObject();
+        if (bullet != null)
         {
             BulletMovement bulletMvmt = bullet.GetComponent<BulletMovement>();
             bullet.transform.position = transform.position;
-            bullet.transform.rotation = transform.rotation;
             bullet.SetActive(true);
-            bulletMvmt.SetDirection(bulletDirection);
+            bulletMvmt.SetDirection(direction);
         }
     }
 
     private void AnimPlayer()
     {
-        anim.SetFloat("LastMoveX", shootSnappedDir.x);
-        anim.SetFloat("LastMoveY", shootSnappedDir.y);
+        anim.SetFloat("LastMoveX", LastDirection.x);
+        anim.SetFloat("LastMoveY", LastDirection.y);
     }
 
     private void DebugLogs()
     {
-        if (debug) 
+        if (debug)
         {
-            Debug.Log($"[PlayerShoot] Shoot direction input = {shootDirection.ReadValue<Vector2>()}");
-            Debug.Log($"[PlayerShoot] Facing direction = {LastDirection}");
+            Debug.Log($"[PlayerShoot] Input mode: {(useGamepad ? "Gamepad" : "Keyboard/Mouse")}");
         }
     }
 }
