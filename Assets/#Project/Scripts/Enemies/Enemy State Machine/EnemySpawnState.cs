40cc69d9c8c1460b2dfa923648b2b0956980e158
Charlotte Mestan <cdmestan@outlook.com>
2024-11-20 19:51:25 +0100

    add fade in enemy spawn state

--- Assets/#Project/Scripts/Enemies/Enemy State Machine/EnemySpawnState.cs
+++ Assets/#Project/Scripts/Enemies/Enemy State Machine/EnemySpawnState.cs
@@ -1,36 +1,49 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemySpawnState : EnemyState
 {
     private SpriteRenderer spriteRenderer;
     private Color originalColor;
+
     public EnemySpawnState(Enemy enemy, EnemyStateMachine enemyStateMachine) : base(enemy, enemyStateMachine)
     {
-        // agent = enemy.Agent;
         stats = enemy.Stats;
-        spriteRenderer = enemy.GetComponent<SpriteRenderer>(); 
+        spriteRenderer = enemy.GetComponent<SpriteRenderer>();
         originalColor = spriteRenderer.color;
     }
+
     public override void EnterState()
     {
         base.EnterState();
-        spriteRenderer.color = Color.black;
-        enemy.StartCoroutine(DurationOfSpawnState());
+
+        spriteRenderer.color = new Color(originalColor.r, originalColor.g, originalColor.b, 0);
+        enemy.StartCoroutine(FadeInCoroutine(2f)); 
     }
+
     public override void ExitState()
     {
         base.ExitState();
-        spriteRenderer.color = originalColor;
+
+        // make sure alpha is set to 1 as we exit the state
+        spriteRenderer.color = new Color(originalColor.r, originalColor.g, originalColor.b, 1f);
     }
-    public override void FrameUpdate()
+
+    private IEnumerator FadeInCoroutine(float duration)
     {
-        base.FrameUpdate();
-    }
-        private IEnumerator DurationOfSpawnState()
+        float elapsedTime = 0f;
+
+        while (elapsedTime < duration)
         {
-            yield return new WaitForSeconds(1f);
-            enemy.StateMachine.ChangeState(enemy.GetStartingState(stats.StartingStateValue));
+            float alpha = Mathf.Lerp(0f, 1f, elapsedTime / duration);
+                // t going from 0/duration = 0 to duration/duration = 1
+            spriteRenderer.color = new Color(originalColor.r, originalColor.g, originalColor.b, alpha);
+            elapsedTime += Time.deltaTime;
+
+            yield return null; 
         }
+
+        // once fade in is complete, switch to first state
+        enemy.StateMachine.ChangeState(enemy.GetStartingState(stats.StartingStateValue));
+    }
 }
