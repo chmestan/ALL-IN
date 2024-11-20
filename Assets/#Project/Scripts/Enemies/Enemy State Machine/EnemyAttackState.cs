493186f10a5444e927b2dcd80dd190d3f4a16f6b
Charlotte Mestan <cdmestan@outlook.com>
2024-11-20 20:39:11 +0100

    add necessary attack stats

--- Assets/#Project/Scripts/Enemies/Enemy State Machine/EnemyAttackState.cs
+++ Assets/#Project/Scripts/Enemies/Enemy State Machine/EnemyAttackState.cs
@@ -6,11 +6,16 @@ public class EnemyAttackState : EnemyState
 {
     public EnemyAttackState(Enemy enemy, EnemyStateMachine enemyStateMachine) : base(enemy, enemyStateMachine)
     {
-        
+        agent = enemy.Agent;
+        stats = enemy.Stats;
     }
     public override void EnterState()
     {
         base.EnterState();
+        if (stats.BulletsPerBurst <= 0) 
+        {
+            Debug.Log($"(EnemyAttackState) The enemy ({enemy.name}) shouldn't be in attack state, yet it apparently is");
+        }
     }
     public override void ExitState()
     {
--- Assets/#Project/Scripts/Enemies/EnemyStats.cs
+++ Assets/#Project/Scripts/Enemies/EnemyStats.cs
@@ -3,31 +3,40 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "EnemyStats", menuName = "ScriptableObjects/EnemyStats", order = 1)]
 public class EnemyStats : ScriptableObject
 {
+    [Header ("HEALTH")]
     [SerializeField] private int _MaxHealth = 10;
-    [SerializeField] private float _MoveSpeed = 3f;
-    [SerializeField] private float _AttackCooldown = 2f;
 
+    [Header ("SPEED"), Space (3f)]
+    [SerializeField] private float _MoveSpeed = 3f;
+
+    [Header ("STATES ATTRIBUTES"), Space (3f)]
     [SerializeField] private EnemyStateEnum _StartingStateValue = EnemyStateEnum.Spawn;
     [SerializeField] private float _RoamingDistance = 3f;
     [SerializeField] private float _ChaseRadius = 2f;
 
+    [Header ("ATTACK STATS"), Space (3f)]
+    [SerializeField] private int _BulletsPerBurst;
+    [SerializeField] private float _TimeBetweenBullets;
+    [SerializeField] private float _TimeBetweenBursts;
+    [SerializeField] private GameObject bulletPrefab;
+
+    #region HEALTH
     public int MaxHealth // read only atm
     {
         get => _MaxHealth;
         // set => maxHealth = value; 
     }
+    #endregion
 
+    #region SPEED
     public float MoveSpeed // read only atm
     {
         get => _MoveSpeed;
         // set => moveSpeed = value; 
     }
+    #endregion
 
-    public float AttackCooldown // read only atm
-    {
-        get => _AttackCooldown;
-        // set => attackCooldown = value;
-    }
+    #region STATES ATTRIBUTES
     public EnemyStateEnum StartingStateValue // read only atm
     {
         get => _StartingStateValue;
@@ -42,5 +51,22 @@ public class EnemyStats : ScriptableObject
     {
         get => _ChaseRadius;
     }
+    #endregion
+
+    #region ATTACK STATS
+    public float BulletsPerBurst
+    {
+        get => _BulletsPerBurst;
+    }
+    public float TimeBetweenBullets
+    {
+        get => _TimeBetweenBullets;
+    }
+    public float TimeBetweenBursts
+    {
+        get => _TimeBetweenBursts;
+    }
+
+    #endregion
 
 }
