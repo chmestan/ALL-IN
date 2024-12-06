29873f51c9acce89d2c787aca49ee2bd1275fa6b
Charlotte Mestan <cdmestan@outlook.com>
2024-12-06 13:10:11 +0100

    fix select and deselect with mouse

---
+++ Assets/#Project/Scripts/ButtonHighlightSelection.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ButtonHighlightSelection : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    [SerializeField] private bool debug = false;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (debug) Debug.Log($"{gameObject.name} hovered");
+        EventSystem.current?.SetSelectedGameObject(gameObject);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (debug) Debug.Log($"{gameObject.name} pointer exited");
+        if (EventSystem.current?.currentSelectedGameObject == gameObject)
+        {
+            if (debug) Debug.Log($"{gameObject.name} deselected");
+            EventSystem.current.SetSelectedGameObject(null);
+        }
+    }
+}
+
+
