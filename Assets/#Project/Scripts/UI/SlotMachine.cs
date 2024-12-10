0d2418b047fdc201f151ea3500e9f8031cc6a7a5
Charlotte Mestan <cdmestan@outlook.com>
2024-12-10 13:31:02 +0100

    slot machine's arm anim working

--- Assets/#Project/Scripts/UI/SlotMachine.cs
+++ Assets/#Project/Scripts/UI/SlotMachine.cs
@@ -1,21 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SlotMachine : MonoBehaviour
 {
-
     private Animator anim;
+    private Button button;
 
     private void Awake()
     {
         anim = GetComponent<Animator>();
+        button = GetComponent<Button>();
     }
 
-    public void OnClick()
+    public void PullAnimation()
     {
+        button.interactable = false;
         anim.SetTrigger("Pull");
+        StartCoroutine(ReEnableButtonAfterAnimation());
     }
 
-
+    private IEnumerator ReEnableButtonAfterAnimation()
+    {
+        yield return new WaitForSeconds(anim.GetCurrentAnimatorStateInfo(0).length);
+        button.interactable = true; 
+    }
 }
